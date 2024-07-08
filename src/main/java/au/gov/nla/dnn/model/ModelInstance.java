d9feb2af917038e9703c08890d7311434a33274a
nla-manderson <168795533+nla-manderson@users.noreply.github.com>
2024-07-08 12:05:10 +1000

    Update ModelInstance.java

--- src/main/java/au/gov/nla/dnn/model/ModelInstance.java
+++ src/main/java/au/gov/nla/dnn/model/ModelInstance.java
@@ -4,9 +4,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.List;
 import java.util.function.Consumer;
-
 import org.json.JSONObject;
-
 import au.gov.nla.dnn.evaluation.ModelEvaluationResult;
 import au.gov.nla.dnn.sequence.SequenceDataRecordProvider;
 import au.gov.nla.dnn.training.TrainingHyperParameters;
@@ -26,7 +24,7 @@ public interface ModelInstance
     
     public interface Builder<T extends ModelInstance>
     {
-        public T create(JSONObject config, long randomSeed) throws Exception;
+        public T create(JSONObject config, int featureCount, int labelCount, long randomSeed) throws Exception;
         public T load(InputStream stream) throws Exception;
     }
 }
