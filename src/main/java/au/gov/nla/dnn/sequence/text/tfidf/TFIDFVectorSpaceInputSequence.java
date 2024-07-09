ced2e411d2c6437ab74f5da6ea995d11628da770
nla-manderson <168795533+nla-manderson@users.noreply.github.com>
2024-07-09 15:00:23 +1000

    Update TFIDFVectorSpaceInputSequence.java

--- src/main/java/au/gov/nla/dnn/sequence/text/tfidf/TFIDFVectorSpaceInputSequence.java
+++ src/main/java/au/gov/nla/dnn/sequence/text/tfidf/TFIDFVectorSpaceInputSequence.java
@@ -1,6 +1,9 @@
 package au.gov.nla.dnn.sequence.text.tfidf;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
 import au.gov.nla.dnn.sequence.InputSequence;
 import au.gov.nla.dnn.sequence.InputSequenceInstance;
@@ -9,8 +12,19 @@ public class TFIDFVectorSpaceInputSequence implements InputSequence
 {
     public InputSequenceInstance generateInstance(List<String> labels, JSONObject config) throws Exception
     {
+        List<String> exclusionPatterns = new ArrayList<String>();
+        JSONArray array = config.getJSONArray("word-exclusion-patterns");
+        
+        for(int i=0; i<array.length(); i++)
+        {
+            exclusionPatterns.add(array.getString(i));
+        }
+        
         return new TFIDFVectorSpaceInputSequenceInstance(
+                config.getString("word-split-pattern"), 
+                exclusionPatterns,
                 config.getInt("word-min-characters"), 
+                config.getInt("word-max-characters"), 
                 config.getInt("word-min-occurrances"), 
                 labels);
     }
